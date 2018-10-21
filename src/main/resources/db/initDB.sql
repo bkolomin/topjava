5eb91bb996d352510ca3a4788d1f011460344432
bkolomin <bkolomin@gmail.com>
2018-10-21 15:01:46 +0400

    users_meals init and populate - done

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS user_meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
@@ -21,5 +22,16 @@ CREATE TABLE user_roles
   user_id INTEGER NOT NULL,
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_meals
+(
+  id              INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  user_id         INTEGER NOT NULL,
+  calories        INTEGER,
+  description     VARCHAR                 NOT NULL,
+  dateTime        TIMESTAMP DEFAULT now() NOT NULL,
+  CONSTRAINT user_meals_roles_idx UNIQUE (user_id, dateTime),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,3 +9,14 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO user_meals (user_id, calories, description, dateTime) VALUES
+  (100000, 500,  'Админ завтрак',  '2018-01-01 09:00:10'),
+  (100000, 1000, 'Админ обед',  '2018-01-01 12:00:20'),
+  (100000, 1500, 'Админ ужин',  '2018-01-01 18:00:30'),
+  (100001, 501,  'Юзер ланч',   '2018-01-01 09:00:40'),
+  (100001, 1001, 'Юзер обед',   '2018-01-01 12:00:50'),
+  (100001, 1501, 'Юзер ужин',   '2018-01-01 18:00:60')
+;
+
+-- select * from user_meals;
